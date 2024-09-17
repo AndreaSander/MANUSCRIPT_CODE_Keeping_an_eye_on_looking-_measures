2b807282d9b57d7a4a89dada9ce97fea0bb1f6ae
AndreaSander <56139512+AndreaSander@users.noreply.github.com>
2024-09-17 15:27:55 -0400

    trying to load relevant peekbank datasets

---
+++ beyond_looks_analyses/loading_cleaning_pkbnk.R
@@ -0,0 +1,14 @@
+
+# remotes::install_github("peekbank/peekbankr")
+library(peekbankr)
+library(tidyselect)
+library(dbplyr)
+
+
+#Get peekbank data 
+
+adams_marchmann<- get_aoi_timepoints(dataset_name = "adams_marchman_2018") %>%
+  left_join(get_administrations(dataset_name ="adams_marchman_2018")) %>%
+  left_join(get_trials(dataset_name = "adams_marchman_2018")) %>%
+  left_join(get_trial_types(dataset_name = "adams_marchman_2018")) %>%
+  left_join(get_stimuli(dataset_name = "adams_marchman_2018"))
